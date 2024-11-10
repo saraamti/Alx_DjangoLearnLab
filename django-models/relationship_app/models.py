4726edb9caad5e481f306f0583dd4d6eb602fe58
sara amti <saraamti0@gmail.com>
2024-11-10 15:25:06 +0100

    Implement Role-Based Access Control

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -28,9 +28,11 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default = 'Member')
- 
-    # Signal to automatically create a UserProfile when a new User is created
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
