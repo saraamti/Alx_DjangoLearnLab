b1d3437b4df9209942b2bb8be25467bd3bdb6d00
sara amti <saraamti0@gmail.com>
2024-11-10 15:53:59 +0100

    Implement Role-Based Access Control

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -21,6 +21,9 @@ class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
 
+  def __str__(self):
+        return self.name
+
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
@@ -28,10 +31,7 @@ class UserProfile(models.Model):
         ('Member', 'Member'),
     ]
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default = 'Member')
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -41,3 +41,4 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library  # Ensure this line includes Library as well as Book
 from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponse
 from .models import UserProfile
 
 
@@ -17,20 +16,27 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-    # View for Admin role
-@user_passes_test(lambda u: u.userprofile.role == 'Admin')
+    def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
 def admin_view(request):
-    return HttpResponse("Welcome to the Admin page!")
+    return render(request, 'relationship_app/admin_view.html')
 
-# View for Librarian role
-@user_passes_test(lambda u: u.userprofile.role == 'Librarian')
+@user_passes_test(is_librarian)
 def librarian_view(request):
-    return HttpResponse("Welcome to the Librarian page!")
+    return render(request, 'relationship_app/librarian_view.html')
 
-# View for Member role
-@user_passes_test(lambda u: u.userprofile.role == 'Member')
+@user_passes_test(is_member)
 def member_view(request):
-    return HttpResponse("Welcome to the Member page!")
+    return render(request, 'relationship_app/member_view.html')
+
 
 
 
