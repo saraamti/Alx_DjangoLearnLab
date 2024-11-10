bd811950fbb2f51f0a3ece61768367c1c27ebd38
sara amti <saraamti0@gmail.com>
2024-11-10 18:42:31 +0100

    Implement Role-Based Access Control

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, register
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
@@ -8,4 +9,7 @@ urlpatterns = [
     path('admin_view/', views.admin_view, name='admin_view'),
     path('librarian_view/', views.librarian_view, name='librarian_view'),
     path('member_view/', views.member_view, name='member_view'),
+    path('login/', LoginView.as_view(template_name= 'relationship_app/login.html'), name = 'login'),
+    path('logout/', LogoutView.as_view(template_name= 'relationship_app/logout.html'), name = 'logout'),
+    path('register/', views.register.as_view(), name = "register"),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library  # Ensure this line includes Library as well as Book
 from django.contrib.auth.decorators import user_passes_test
-from .models import UserProfile
+from .models import UserProfile , Book
+from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Function-based view to list all books
@@ -15,26 +18,29 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-def __str__(self):
-        return f"{self.user.username} - {self.role}"
 
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+   def get_object(self):
+        return Library.objects.get(name = "Cairo")
 
-def is_member(user):
-    return user.userprofile.role == 'Member'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()
+        books = library.books.all()
+        context['books'] = books
+        return context
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the user after registration
+            return redirect(reverse_lazy('login'))  # Redirect to the login page
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'relationship_app/register.html', {'form': form})
 
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
 
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
 
 
 
