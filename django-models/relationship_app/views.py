6eb98877b5930e8df5e925125a4f93c85cf1ad1e
sara amti <saraamti0@gmail.com>
2024-11-10 19:38:52 +0100

    Implement Role-Based Access Control

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 
+
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
@@ -39,6 +40,29 @@ def register(request):
         form = UserCreationForm()
     
     return render(request, 'relationship_app/register.html', {'form': form})
+
+# views.py
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 
 
