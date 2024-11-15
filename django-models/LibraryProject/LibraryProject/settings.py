2bf5c3920f8e8541bdf0473a4e81b6fc8e7e03d8
sara amti <saraamti0@gmail.com>
2024-11-15 22:51:40 +0100

    last check

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',
-    'relationship_app.apps.RelationshipAppConfig' ,
+    'relationship_app.apps.RelationshipAppConfig'
      
 ]
 
