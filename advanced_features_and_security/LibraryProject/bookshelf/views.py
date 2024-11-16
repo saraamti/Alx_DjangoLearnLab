a7c7176d08d480750f31aca610041794e37b12b3
sara amti <saraamti0@gmail.com>
2024-11-16 14:44:01 +0100

    third projrct

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from .models import Book
+from .forms import ExampleForm
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, book_id):
