c1a03c686025aeac4eaf18728784c7a60e05452c
sara amti <saraamti0@gmail.com>
2024-11-25 21:36:15 +0100

    cleart tghe url app

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,21 @@
-from rest_framework.generics import ListAPIView
+from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 
-class BookList(ListAPIView):  # Explicitly using ListAPIView
-    """
-    API view to retrieve a list of books.
-    """
+# Create your views here.
+# class BookList(generics.ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
 
 
