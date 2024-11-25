3c37c5763aa250c28bf2dba49e43ce290028ed82
sara amti <saraamti0@gmail.com>
2024-11-25 21:34:13 +0100

    cleart tghe url app

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListAPIView):
+class BookList(ListAPIView):  # Explicitly using ListAPIView
+    """
+    API view to retrieve a list of books.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
 
 
