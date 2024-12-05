b0794ffffcdf18a77b008b6dfeef6f89e9a3a8c7
sara amti <saraamti0@gmail.com>
2024-12-05 16:41:57 +0100

    check

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -83,8 +83,11 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
-}DATABASES = {
+}
+DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'your_db_name',
         'USER': 'your_username',
