231e8d695d6a9c381b9af4219b18dccd5a33af5b
sara amti <saraamti0@gmail.com>
2024-12-14 23:15:57 +0100

    lets try

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserProfileView
+from .views import LoginView, RegisterView, ProfileView, FollowUserView, UnfollowUserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('profile/', UserProfileView.as_view(), name='profile')
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user')
 ]
-from django.urls import path
-from .views import FollowViewSet
 
-urlpatterns = [
-    path('follow/<int:pk>/', FollowViewSet.as_view({'post': 'follow'}), name='follow'),
-    path('unfollow/<int:pk>/', FollowViewSet.as_view({'post': 'unfollow'}), name='unfollow'),
-]
